764ee60d5fcfba69cb791e7d24f5bf0ba060f480
Javi Sabio <javi.sabio@gmail.com>
2014-07-12 20:31:59 +0100

    commit

---
+++ plot2.R
@@ -0,0 +1,30 @@
+plot2 <- function(path, filename)
+{
+  #Read data for the two dates
+  fullfile = paste(path, "/", sep = "")
+  fullfile = paste(fullfile, filename, sep = "")
+  input <- read.table(fullfile, sep = ";", header = TRUE, na.strings = "?")
+  
+  data_01 <- input[input$Date == "1/2/2007",]
+  data_02 <- input[input$Date == "2/2/2007",]
+  
+  #Take Global Active Power for the two days
+  total_active_pwr <- c(date_01$Global_active_power, date_02$Global_active_power)
+  
+  #Create data for time merging dates and times and converting to R time class
+  time_01 <- paste(date_01$Date, date_01$Time)
+  time_02 <- paste(date_02$Date, date_02$Time)
+  
+  total_time <- c(time_01, time_02)  
+  total_time <- strptime(total_time, "%d/%m/%Y %H:%M:%S")
+  
+  #Build plot in plot2.png
+  png(file = "plot2.png")
+  plot(total_time, total_active_pwr, type = "n", xlab = "", ylab = "Global Active Power (kilowatts)")
+  lines(total_time, total_active_pwr)
+  dev.off()
+}
+
+
+
+
